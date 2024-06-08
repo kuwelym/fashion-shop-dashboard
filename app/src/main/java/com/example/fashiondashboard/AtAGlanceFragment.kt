a1630cfd3f00fb34b7a3592eb2e217440f8b6a97
kuwelym <klyminh@gmail.com>
2024-06-08 21:26:29 +0700

    initial commit

---
+++ app/src/main/java/com/example/fashiondashboard/AtAGlanceFragment.kt
@@ -0,0 +1,17 @@
+package com.example.fashiondashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AtAGlanceFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_at_a_glance, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/fashiondashboard/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.fashiondashboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        bottomNavigationView = findViewById(R.id.bottomNavigation)
+        bottomNavigationView.selectedItemId = R.id.atAGlanceBotNav
+        val navController = supportFragmentManager.findFragmentById(R.id.fragmentContainer)!!.findNavController()
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
+}
